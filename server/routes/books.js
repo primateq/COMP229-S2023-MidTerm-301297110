f3b200d8ab595216ac51720d1e30a9ed945b6122
Alessandra Primatesta <alessandra.primatesta@gmail.com>
2023-06-21 15:05:01 -0400

    buttons added to book list page

--- server/routes/books.js
+++ server/routes/books.js
@@ -23,47 +23,90 @@ router.get('/', (req, res, next) => {
 
 });
 
-//  GET the Book Details page in order to add a new Book
-router.get('/add', (req, res, next) => {
+//  GET the Book Details page in order to add a new Book - CREATE Operation
+router.get('/details', async (req, res, next) => {
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+    try {
+      res.render('books/details', {title: 'Books'})
+    } catch (err) {
+      console.log(err);
+    }
 
 });
 
-// POST process the Book Details page and create a new Book - CREATE
-router.post('/add', (req, res, next) => {
+// POST process the Book Details page and create a new Book - UPDATE Operation
+router.post('/details', async (req, res, next) => {
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+     let newBook = new Book ({
+      "Title": req.body.Title,
+      "Description": req.body.Descrption,
+      "Price": req.body.Price,
+      "Author": req.body.Author,
+      "Genre": req.body.Genre
+     });
 
+     try {
+          await newBook.save();
+          res.redirect('/books')
+        }
+     catch (err) {
+          console.log(err);
+          res.status(500).send(err);
+     }
 });
 
 // GET the Book Details page in order to edit an existing Book
-router.get('/:id', (req, res, next) => {
+router.get('/details/:id', async (req, res, next) => {
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+    let id = req.params.id;
+
+    try {
+      let bookToEdit = await books.findById(id);
+      res.render('books/details', {title: 'Edit Book', book: bookToEdit});
+    }
+    catch (err) {
+          console.log(err);
+          res.status(500).send(err);
+    }
 });
 
 // POST - process the information passed from the details form and update the document
-router.post('/:id', (req, res, next) => {
+router.post('/detials/:id', async (req, res, next) => {
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+  let id = req.params.id;
+
+  let updatedBook = {
+    "Title": req.body.Title,
+    "Description": req.body.Descrption,
+    "Price": req.body.Price,
+    "Author": req.body.Author,
+    "Genre": req.body.Genre
+   }
+
+   try {
+    await book.updateOne({_id: id}, updatedBook);
+    res.render('/books');
+  }
+  catch (err) {
+        console.log(err);
+        res.status(500).send(err);
+  }
 
 });
 
 // GET - process the delete by user id
-router.get('/delete/:id', (req, res, next) => {
+router.get('/delete/:id', async (req, res, next) => {
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+    let id = req.params.id;
+
+    try {
+      await book.findByIdAndRemove(id);
+      res.redirect('/books');
+    }
+    catch (err) {
+      console.log(err);
+      res.status(500).send(err);
+    }
 });
 
 
