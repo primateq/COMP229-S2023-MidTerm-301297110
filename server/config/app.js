d436063c0d8657e8b5164a5e9d35e1c8ca32159c
Alessandra Primatesta <alessandra.primatesta@gmail.com>
2023-06-21 11:22:16 -0400

    small updates to files and ensure DB is connected

--- server/config/app.js
+++ server/config/app.js
@@ -35,6 +35,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../../client')));
+app.use(express.static(path.join(__dirname, '../../node_modules')));
 
 
 // route redirects
