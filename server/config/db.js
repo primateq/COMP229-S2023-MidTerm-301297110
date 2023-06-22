885e6811df4e77fdb4d558588da8156a5a76e570
Alessandra Primatesta <alessandra.primatesta@gmail.com>
2023-06-22 11:50:03 -0400

    update to db URL

--- server/config/db.js
+++ server/config/db.js
@@ -5,5 +5,7 @@ Due date: June 25, 2023*/
 
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  //"URI": "mongodb://localhost/books229"
+
+  "URI": "mongodb+srv://alessandraprimatesta:password4@mongodbserver.um6z3ko.mongodb.net/books229?retryWrites=true&w=majority"
 };
