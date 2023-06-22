74a94483ad983551cc2ac71eacdd6e88791977aa
Alessandra Primatesta <alessandra.primatesta@gmail.com>
2023-06-22 09:29:25 -0400

    fully functional local site with comments

--- server/config/db.js
+++ server/config/db.js
@@ -1,3 +1,8 @@
+/*File name: comp229-s2023-midterm-301297110
+Student Name: Alessandra Primatesta
+Student ID: 301.297.110
+Due date: June 25, 2023*/
+
 module.exports = {
   //local MongoDB deployment ->
   "URI": "mongodb://localhost/books229"
